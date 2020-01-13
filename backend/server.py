ad62876b380f945b8c9134683985475895ee0056
Larry Cohen <cohlar@gmail.com>
2020-01-13 17:58:26 +0200

    use marshmallow to deserialize sqlalchemy objects

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from datetime import datetime
-import json
-import logging
+import json, logging
 
 magicskills = ['Alchemy', 'Animation', 'Conjuror', 'Disintegration', 'Elemental', 'Healing', 'Illusion', 'Immortality', 'Invisibility',
                'Invulnerability', 'Necromancer', 'Omnipresent', 'Omniscient', 'Poison', 'Possession', 'Self-detonation', 'Summoning', 'Water breathing']
@@ -18,6 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hogwarts.db'
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 class Student(db.Model):
@@ -26,6 +27,8 @@ class Student(db.Model):
     lastname = db.Column(db.String(20), nullable=False)
     created = db.Column(db.DateTime, default=datetime.utcnow)
     lastupdated = db.Column(db.DateTime, default=datetime.utcnow)
+    magicskills = db.relationship('StudentSkill', backref='author', lazy=True)
+    courses = db.relationship('StudentCourse', backref='author', lazy=True)
 
 
 class StudentSkill(db.Model):
@@ -42,6 +45,11 @@ class StudentCourse(db.Model):
     studentid = db.Column(db.Integer, db.ForeignKey(
         'student.id'), nullable=False)
     course = db.Column(db.String(50), nullable=False)
+    
+    
+class StudentSchema(ma.ModelSchema):
+    class Meta:
+        model = Student
 
 
 @app.route('/')
@@ -54,23 +62,25 @@ def create():
     student = request.get_json()
 
     firstname = student.get('firstname')
-    lastname = student.get('lastname')    
+    lastname = student.get('lastname')
     if not firstname or not lastname:
-        abort(404) # add message '{"message": "First Name and Last Name are both mandatory"}'
-        
+        # add message '{"message": "First Name and Last Name are both mandatory"}'
+        abort(404)
+
     new_student = Student(firstname=firstname, lastname=lastname)
     db.session.add(new_student)
     db.session.commit()
 
     second_commit = False
-    
+
     magicskills = student.get('magicskills')
     for magicskill in magicskills:
         second_commit = True
         skill = magicskill.get('skill')
         skilltype = magicskill.get('skilltype')
         level = magicskill.get('level')
-        new_skill = StudentSkill(studentid=new_student.id, skill=skill, skilltype=skilltype, level=level)
+        new_skill = StudentSkill(
+            studentid=new_student.id, skill=skill, skilltype=skilltype, level=level)
         db.session.add(new_skill)
 
     courses = student.get('courses')
@@ -78,39 +88,29 @@ def create():
         second_commit = True
         new_course = StudentCourse(studentid=new_student.id, course=course)
         db.session.add(new_course)
-    
+
     if second_commit:
         db.session.commit()
-        
+
     return 'Added new student'
 
 
 @app.route('/api/students/get')
 def get():
     students = db.session.query(Student).all()
-    output = []
-    for student in students:
-        output.append({
-            'id': student.id,
-            'firstname': student.firstname,
-            'lastname': student.lastname,
-            'created': student.created.strftime("%d/%m/%Y %H:%M:%S"),
-            'lastupdated': student.lastupdated.strftime("%d/%m/%Y %H:%M:%S"),
-        })
-    return json.dumps(output)
+    student_schema = StudentSchema(many=True)
+    output = student_schema.dump(students)
+    return jsonify(output)
 
 
 @app.route('/api/students/get/<id>')
 def getbyid(id):
     student = db.session.query(Student).get(id)
-    output = {
-        'id': student.id,
-        'firstname': student.firstname,
-        'lastname': student.lastname,
-        'created': student.created.strftime("%d/%m/%Y %H:%M:%S"),
-        'lastupdated': student.lastupdated.strftime("%d/%m/%Y %H:%M:%S"),
-    }
-    return json.dumps(output)
+    student_schema = StudentSchema()
+    output = student_schema.dump(student)
+    # print(student.magicskills)
+    # print(student.courses)
+    return jsonify(output)
 
 
 if __name__ == '__main__':
